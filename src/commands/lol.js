0df4c64b780e7e2aaa7117e6bc296964bb1947df
Enzo Arauco <enzoarauco@gmail.com>
2021-12-07 13:54:53 -0500

    fix lol commands

--- src/commands/lol.js
+++ src/commands/lol.js
@@ -5,10 +5,13 @@ module.exports = async (client, message, args) => {
     const res = await fetch(`https://la2.api.riotgames.com/lol/status/v4/platform-data?api_key=${riot_key}`)
     const data = await res.json();
     let i = 0;
-    if(data.incidents == 0) {
+    let j = 0;
+    if(data.incidents.length == 0) {
       message.channel.send('No hay nuevos incidentes.')
     } else {
       do {
+        const title = data.incidents[i].titles.find(el => el.locale == "es_AR");
+        const update = data.incidents[i].updates[0].translations.find(el => el.locale == "es_AR")
         message.channel.send({
           embed: {
               color: 'RED',
@@ -19,8 +22,8 @@ module.exports = async (client, message, args) => {
               },
               fields: [
                 {
-                  name: data.incidents[i].titles[1].content,
-                  value: data.incidents[i].updates[0].translations[1].content
+                  name: `${title["content"]}`,
+                  value: `${update["content"]}`
                 }
               ],
               timestamp: data.incidents[i].updates[0]?.updated_at
@@ -34,13 +37,14 @@ module.exports = async (client, message, args) => {
       message.channel.send("No hay mantenimiento en curso.");
     } else {
       do {
-        const title = data.maintenances[i].titles.find(el => el.locale == "es_AR");
-        const update = data.maintenances[i].updates[i].translations.find(el => el.locale == "es_AR")
+        const title = data.maintenances[j].titles.find(el => el.locale == "es_AR");
+        const update = data.maintenances[j].updates[0].translations.find(el => el.locale == "es_AR")
+        console.log(data.maintenances[j].titles.find(el => el.locale == "es_AR"));
         message.channel.send({
           embed: {
             color: 'YELLOW',
-            title: `Mantenimiento #${i+1}`,
-            description: `Severidad: ${data.maintenances[i].incident_severity}`,
+            title: `Mantenimiento #${j+1}`,
+            description: `Severidad: ${data.maintenances[j].incident_severity}`,
             thumbnail: {
               url: 'https://static.wikia.nocookie.net/leagueoflegendsoficial/images/8/8c/LOL_Logo.png/revision/latest?cb=20180119195439&path-prefix=es'
             },
@@ -50,10 +54,10 @@ module.exports = async (client, message, args) => {
                 value: `${update["content"]}`
               }
             ],
-            timestamp: data.maintenances[i].updates[i]?.updated_at
+            timestamp: data.maintenances[j].updates[j]?.updated_at
           }
         })
-      i++
+      j++
       } while (i < data.incidents.length)
     }
   } catch (error) {
